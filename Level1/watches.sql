3da6b5fbd3c074f612f4697895f427939613166d
Mindy Garcia <96347335+mingarci@users.noreply.github.com>
2022-05-26 16:36:19 -0500

    SQL Queries

---
+++ Level1/watches.sql
@@ -0,0 +1,24 @@
+select  *
+from review_id_table
+where review_date between '2015-01-01' and '2020-01-31';
+
+SELECT * FROM products
+LIMIT 50;
+
+SELECT COUNT(*) FROM products;
+
+SELECT * FROM customers
+LIMIT 50;
+
+SELECT COUNT(*) FROM customers;
+
+SELECT * FROM review_id_table
+LIMIT 50;
+
+SELECT COUNT(*) FROM review_id_table;
+
+SELECT * FROM vine_table
+LIMIT 50;
+
+SELECT COUNT(*) FROM vine_table;
+LIMIT 50;
